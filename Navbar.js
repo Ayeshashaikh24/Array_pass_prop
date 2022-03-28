db0be48000182c1a5340aeaacad618d981a3d013
Ayeshashaikh24 <97972259+Ayeshashaikh24@users.noreply.github.com>
2022-03-28 19:58:43 +0530

    Add files via upload

---
+++ Navbar.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+
+ function Navbar(props) {
+  return (
+    <>
+      <h1> name of students are:{props.name}</h1>
+    </>
+
+  );
+           }
+           export default Navbar;

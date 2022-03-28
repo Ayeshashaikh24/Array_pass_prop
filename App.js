6a577db93e3a6cb3c49f18d82a810e249af84619
Ayeshashaikh24 <97972259+Ayeshashaikh24@users.noreply.github.com>
2022-03-28 19:58:23 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,18 @@
+// import logo from './logo.sv
+import React  from 'react';
+import Button from '@mui/material/Button';
+import AddIcon from '@mui/icons-material/Add';
+import { useState } from 'react';
+import Navbar from './components/Navbar'
+function App (){
+return(
+<>
+<Navbar name="['a','b',c] ">
+</Navbar>
+
+ </>
+    );
+  }
+  export default App;
+  
+

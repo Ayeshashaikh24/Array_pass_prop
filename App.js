8775c7962fe152270b5c99c8bf5fa44d11664ea7
Ayeshashaikh24 <97972259+Ayeshashaikh24@users.noreply.github.com>
2022-03-29 19:46:06 +0530

    Add files via upload

--- App.js
+++ App.js
@@ -1,12 +1,13 @@
 // import logo from './logo.sv
 import React  from 'react';
-
+import Button from '@mui/material/Button';
+import AddIcon from '@mui/icons-material/Add';
 import { useState } from 'react';
 import Navbar from './components/Navbar'
 function App (){
 return(
 <>
-<Navbar name="['a','b',c] ">
+<Navbar name={["a","b","c","d"]} >
 </Navbar>
 
  </>
--- Navbar.js
+++ Navbar.js
@@ -2,9 +2,11 @@ import React from 'react'
 
 
  function Navbar(props) {
+   const name=props.name;
+   const listItems=name.map((names)=><li>{names}</li>);
   return (
     <>
-      <h1> name of students are:{props.name}</h1>
+      <h1> name of students are:{listItems}</h1>
     </>
 
   );
